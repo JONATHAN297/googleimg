40d4a8a8eb8e8ba1813fe4f503a76e044ed554bc
JONATHAN297 <72265751+JONATHAN297@users.noreply.github.com>
2020-10-02 15:20:03 -0500

    Add files via upload

---
+++ contenido.php
@@ -0,0 +1,29 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/tarafres.png"."||".
+				  "FRESAMICH"."||".
+				    "Preparado de michelada sabor FRESA, presentacion de 1/2 kilo, tambien contamos con presentacion de a 1 kilo . Su costo a publico mayorista es de $27 por caja con 20 pz de 1/2 kilo ,precio menudeo es de $37 ,su costo de 1 kilo  es de $47 mayoreo con 16 pz, menudeo $67 . Contamos con otros sabores y otros productos.";
+
+		$datos[1]="img/mangoo.jpg"."||".
+				  "MANGOMICH"."||".
+				   "Preparado de michelada sabor MANGO, presentacion de 1/2 kilo, contamos con presentacion de a 1 kilo . Su costo a publico mayorista es de $27 por caja con 20 pz de 1/2 kilo ,precio menudeo es de $37 ,su costo de 1 kilo  es de $47 mayoreo con 16 pz, menudeo $67 . Contamos con otros sabores y otros productos.";
+
+		$datos[2]="img/ARANDANO.jpg"."||".
+				  "ARANDANOMICH"."||".
+				   "Preparado de michelada sabor ARANDANO, presentacion de 1/2 kilo, tambien contamos con presentacion de a 1 kilo . Su costo a publico mayorista es de $27 por caja con 20 pz de 1/2 kilo ,precio menudeo es de $37 ,su costo de 1 kilo  es de $47 mayoreo con 16 pz, menudeo $67 . Contamos con otros sabores y otros productos.";
+
+
+		$datos[3]="img/TARA.jpg"."||".
+				  "TARAMINDO"."||".
+				  "Preparado de michelada sabor TAMARINDO, presentacion de 1/2 kilo ,contamos con presentacion de a 1 kilo . Su costo a publico mayorista es de $23 por caja con 20 pz de 1/2 kilo ,precio menudeo es de $35 ,su costo de 1 kilo  es de $43 mayoreo con 16 pz, menudeo $67 . Contamos con otros sabores y otros productos.";
+
+		$datos[4]="img/mora aazul.jpg"."||".
+				  "MORAMICH"."||".
+				   "Preparado de michelada sabor MORA AZUL, presentacion de 1/2 kilo,trabajamos tambien presentacion de a 1 kilo . Su costo a publico mayorista es de $27 por caja con 20 pz de 1/2 kilo ,precio menudeo es de $37 ,su costo de 1 kilo  es de $47 mayoreo con 16 pz, menudeo $67 . Contamos con otros sabores y otros productos .";
+	return $datos;
+	}
+
+ ?>
